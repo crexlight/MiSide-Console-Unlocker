54cc0436ad9a1589668cf18382ea181c06ab9a05
Rist8 <94626383+Rist8@users.noreply.github.com>
2024-12-11 22:31:01 +0100

    Add files via upload

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Plugin : MonoBehaviour{
+	private void Start(){
+        ConsoleMain.active = true;
+    }
+	
+	void Update(){
+        if (UnityEngine.Input.GetKeyDown(KeyCode.F9)){
+			if (Time.timeScale != 0.0f)
+				Time.timeScale = 0.0f;
+			else
+				Time.timeScale = 1.0f;
+        }
+	}
+}
---
+++ Plugin/PluginLoader.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+public static class PluginInfo
+{
+	public const string PLUGIN_GUID = "UniversalAssetLoader";
+	public const string PLUGIN_NAME = "Universal Asset Loader";
+	public const string PLUGIN_VERSION = "1.1.0";
+
+	public static PluginLoader Instance;
+	public static string AssetsFolder = Paths.PluginPath + "\\" + PluginInfo.PLUGIN_GUID + "\\Assets";
+}
+
+[BepInPlugin("org.miside.plugins.presentdaypresenttime", PluginInfo.PLUGIN_NAME, "1.1.0")]
+public class PluginLoader : BasePlugin
+{
+	public ManualLogSource Logger { get; private set; }
+
+	public PluginLoader() {}
+
+	public override void Load()
+	{
+		Logger = (this as BasePlugin).Log;
+		PluginInfo.Instance = this;
+		IL2CPPChainloader.AddUnityComponent(typeof(Plugin));
+	}
+}
