68c6546b45865079920a5506a44a38a8b2d40c62
Rist8 <94626383+Rist8@users.noreply.github.com>
2024-12-14 12:09:17 +0100

    1.1.0 version upload

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -1,25 +1,55 @@
+﻿using UnityEditor;
 using UnityEngine;
 
-public class Plugin : MonoBehaviour
+public class ConsoleUnlocker : MonoBehaviour
 {
+
+    static CapsuleCollider showerCapsule = null;
+    static GameObject blur = null;
+
     private void Start()
 	{
         ConsoleMain.active = true;
+		
     }
-	
+
 	void Update()
 	{
+
 		if (ConsoleMain.liteVersion)
 		{
 			ConsoleMain.liteVersion = false;
 		}
-        if (UnityEngine.Input.GetKeyDown(KeyCode.F9))
+		if (UnityEngine.Input.GetKeyDown(KeyCode.U))
 		{
+			if (Application.targetFrameRate != -1)
+			{
+				Application.targetFrameRate = -1;
+				QualitySettings.vSyncCount = 0;
+			}
+			else
+			{
+                Application.targetFrameRate = 60;
+                QualitySettings.vSyncCount = 1;
+			}
+        }
+        if (UnityEngine.Input.GetKeyDown(KeyCode.F9)){
 			if (Time.timeScale != 0.0f)
 				Time.timeScale = 0.0f;
 			else
 				Time.timeScale = 1.0f;
         }
+
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Slash))
+        {
+            if (Reflection.FindObjectsOfType<ConsoleCall>(true).Length > 0)
+            {
+                ConsoleCall camerafly = Reflection.FindObjectsOfType<ConsoleCall>(true)[0];
+                if (camerafly)
+                {
+                    camerafly.CameraFly();
+                }
+            }
+        }
     }
-	
 }
